713393e763e2a13eb93fc73b1b2731a5c91d06ac
mrkevr <markevercantillano@gmail.com>
2024-09-18 19:26:36 +0800

    init

---
+++ src/main/java/dev/mrkevr/bebe/BebeApplication.java
@@ -0,0 +1,13 @@
+package dev.mrkevr.bebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebeApplication.class, args);
+	}
+	
+}
